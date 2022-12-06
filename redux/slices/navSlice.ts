228338cc8628d663f44bf130f870a5914281f27f
Mihai-Vlăduț STOICA <stoicavladut18@gmail.com>
2022-12-06 16:57:50 +0200

    Update Redux store

---
+++ redux/slices/navSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null
+}
+
+export const navSlice = createSlice({
+    name: "nav",
+    initialState: initialState,
+    reducers: {
+        setOrigin: (state, action) => {
+            state.origin = action.payload
+        },
+        setDestination: (state, action) => {
+            state.destination = action.payload
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload
+        }
+    }
+})
+
+export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions
+export default navSlice.reducer;
--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import navSlice from "./slices/navSlice";
 
 export default configureStore({
-    reducer: {}
+    reducer: {
+        nav: navSlice
+    }
 })
