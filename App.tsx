501360b8a883a912ec527da95efe8630b1627bcd
Mihai-Vlăduț STOICA <stoicavladut18@gmail.com>
2022-12-06 16:30:51 +0200

    Init commit for Redux

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import store from './redux/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Let's build an Uber clone app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
