7fff29dfaae541f3aa9bdca5f4868dbc60a11569
Mihai-Vlăduț STOICA <stoicavladut18@gmail.com>
2022-12-12 23:25:35 +0200

    Implement MapScreen.tsx

--- App.tsx
+++ App.tsx
@@ -2,11 +2,34 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import HomeScreen from './screens/HomeScreen';
+import {NavigationContainer} from "@react-navigation/native";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import MapScreen from "./screens/MapScreen";
+import LocationSearchModal from "./modals/LocationSearchModal";
 
 export default function App() {
+    const Stack = createNativeStackNavigator();
+
     return (
         <Provider store={store}>
-            <HomeScreen/>
+            <NavigationContainer>
+                <SafeAreaProvider>
+                    <Stack.Navigator screenOptions={{headerShown: false}}>
+                        <Stack.Screen
+                            name="HomeScreen"
+                            component={HomeScreen}/>
+                        <Stack.Screen
+                            name="MapScreen"
+                            component={MapScreen}/>
+                        <Stack.Group screenOptions={{presentation: "containedModal"}}>
+                            <Stack.Screen
+                                name={"LocationSearchModal"}
+                                component={LocationSearchModal}/>
+                        </Stack.Group>
+                    </Stack.Navigator>
+                </SafeAreaProvider>
+            </NavigationContainer>
         </Provider>
     );
 }
---
+++ screens/MapScreen.tsx
@@ -0,0 +1,92 @@
+import {Pressable, SafeAreaView, StyleSheet, Text, View} from "react-native";
+import Map from "@components/Map";
+import {useNavigation} from "@react-navigation/native";
+import LocationSearchModal from "../modals/LocationSearchModal";
+import GoBackButton from "@components/GoBackButton";
+
+const MapScreen = () => {
+    const navigation = useNavigation();
+
+    return (
+        <>
+            <Map/>
+            <SafeAreaView>
+                    <GoBackButton/>
+            </SafeAreaView>
+            
+            <View style={{backgroundColor:"#fff", position: "absolute", bottom: 0, right: 0, left: 0}}>
+                <SafeAreaView>
+                    <View style={{backgroundColor:"#fff"}}>
+                        <View style={styles.headerWrapper}>
+                            <Text style={styles.headerText}>Set your pick-up location</Text>
+                        </View>
+                        <View style={styles.actionsWrapper}>
+                            <Pressable style={styles.searchButton} onPress={() => navigation.navigate("LocationSearchModal")}>
+                                <Text style={styles.headerText}>Move the map</Text>
+                                <View style={styles.searchView}>
+                                    <Text style={styles.searchText}>Search</Text>
+                                </View>
+                            </Pressable>
+                            <Pressable disabled={true}>
+                                <View style={styles.confirmButton}>
+                                    <Text style={styles.confirmText}>Confirm pick-up</Text>
+                                </View>
+                            </Pressable>
+                        </View>
+                    </View>
+                </SafeAreaView>
+            </View>
+        </>
+    )
+}
+
+export default MapScreen;
+
+const styles = StyleSheet.create({
+    headerWrapper: {
+        padding: 14,
+        
+        borderBottomWidth: 2,
+        borderBottomColor: "#d4d4d4",
+        
+    },
+    headerText: {
+        fontSize: 16,
+        fontWeight: "500",
+        textAlign: "center",
+    },
+    actionsWrapper: {
+        paddingHorizontal: 22,
+    },
+    searchButton: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        
+        paddingVertical: 16,
+    },
+    searchView: {
+        paddingHorizontal: 12,
+        paddingVertical: 8,
+        
+        backgroundColor: "#e5e5e5",
+        
+        borderRadius: 999,
+    },
+    searchText: {
+        fontWeight: "500",
+    },
+    confirmButton: {
+        paddingVertical: 18,
+        backgroundColor: "#000",
+    },
+    
+    confirmText: {
+        color: "#fff",
+        
+        fontSize: 18,
+        fontWeight: "500",
+        
+        textAlign: "center",
+    }
+})
