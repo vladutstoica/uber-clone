fef3bb6c008be6b3cdf85dc90075355ca2751b1a
Mihai-Vlăduț STOICA <stoicavladut18@gmail.com>
2022-12-12 23:24:36 +0200

    Init setup for Map.tsx

---
+++ components/Map.tsx
@@ -0,0 +1,22 @@
+import MapView from "react-native-maps";
+
+const Map = () => {
+    return (
+        <MapView
+            style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right:0,
+                bottom:0,
+            }}
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}/>
+    )
+}
+
+export default Map;
